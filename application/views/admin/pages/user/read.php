ac1c2d5cd5949cc95cb87c362b0c83fbf7006343
lutfiangga <lutfiangga2908@gmail.com>
2024-09-26 09:13:10 +0700

    datatble

--- application/views/admin/pages/user/read.php
+++ application/views/admin/pages/user/read.php
@@ -46,7 +46,92 @@
 		<a href="<?= site_url('admin/user/create'); ?>" class="btn bg-blue-600 border-none text-[#fafafa] hover:bg-[#fafafa]/30 hover:text-blue-600 hover:border-2 hover:border-blue-600 hover:shadow-md mb-4 w-full md:w-auto">Tambah Pengguna</a>
 
 		<div class="overflow-x-auto">
-			<table class="min-w-full table-auto table-data">
+			<table id="" class="min-w-full table-auto table-data">
+				<thead class="bg-gray-100">
+					<tr>
+						<th class="px-4 py-2">No</th>
+						<th class="px-4 py-2">Nama</th>
+						<th class="px-4 py-2">Email</th>
+						<th class="px-4 py-2">Status</th>
+						<th class="px-4 py-2">Aksi</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="border-t">
+						<td class="px-4 py-2">2</td>
+						<td class="px-4 py-2">
+							<div class="flex flex-row gap-2 items-center">
+								<img src="https://via.placeholder.com/40" alt="role" class="rounded-full w-8 h-8">
+								<div class="flex flex-col items-center justify-center">
+									<p class="truncate w-full ml-2 font-semibold">Mahasiswa aktif tahun ajaran 2020</p>
+									<p class="truncate w-full ml-2 text-[0.6rem] tracking-wide uppercase">sistem informasi - 32132112</p>
+								</div>
+							</div>
+						</td>
+						<td class="px-4 py-2 whitespace-nowrap">almaata@almaata.ac.id</td>
+						<td>
+							<span class="flex items-center justify-center cursor-default text-sm gap-2 text-green-600 hover:bg-[#EEF0F6] py-2 rounded-full">
+								<i data-feather="check-circle" class="w-4 h-auto"></i>
+								Active
+							</span>
+						</td>
+						<td class="px-4 py-2 flex flex-row items-center justify-center mt-2 gap-2">
+							<a href="#" class="bg-green-600 rounded-full p-2 text-[#fafafa] hover:px-4 flex items-center gap-2 group">
+								<i data-feather="edit" class="w-4 h-auto"></i>
+								<p class="hidden group-hover:block text-white transition-opacity duration-300">Edit</p>
+							</a>
+							<a href="#" class="bg-red-600 rounded-full p-2 text-[#fafafa] hover:px-4 flex items-center gap-2 group">
+								<i data-feather="trash-2" class="w-4 h-auto"></i>
+								<p class="hidden group-hover:block text-white transition-opacity duration-300">Hapus</p>
+							</a>
+
+						</td>
+					</tr>
+					<tr class="border-t">
+						<td class="px-4 py-2">1</td>
+						<td class="px-4 py-2">
+							<div class="flex flex-row gap-2 items-center">
+								<img src="https://via.placeholder.com/40" alt="role" class="rounded-full w-8 h-8">
+								<div class="flex flex-col items-center justify-center">
+									<p class="truncate w-full ml-2 font-semibold">Mahasiswa aktif tahun ajaran 2020</p>
+									<p class="truncate w-full ml-2 text-[0.6rem] tracking-wide uppercase">sistem informasi - 32132112</p>
+								</div>
+							</div>
+						</td>
+						<td class="px-4 py-2 whitespace-nowrap">almaata@almaata.ac.id</td>
+						<td>
+							<span class="flex items-center justify-center cursor-default text-sm gap-2 text-red-600 hover:bg-[#EEF0F6] py-2 rounded-full">
+								<i data-feather="x-circle" class="w-4 h-auto"></i>
+								Inactive
+							</span>
+						</td>
+						<td class="px-4 py-2 flex flex-row items-center justify-center mt-2 gap-2">
+							<a href="#" class="bg-green-600 rounded-full p-2 text-[#fafafa] hover:px-4 flex items-center gap-2 group">
+								<i data-feather="edit" class="w-4 h-auto"></i>
+								<p class="hidden group-hover:block text-white transition-opacity duration-300">Edit</p>
+							</a>
+							<a href="#" class="bg-red-600 rounded-full p-2 text-[#fafafa] hover:px-4 flex items-center gap-2 group">
+								<i data-feather="trash-2" class="w-4 h-auto"></i>
+								<p class="hidden group-hover:block text-white transition-opacity duration-300">Hapus</p>
+							</a>
+
+						</td>
+					</tr>
+				</tbody>
+				<tfoot class="bg-gray-100">
+					<tr>
+						<th class="px-4 py-2">No</th>
+						<th class="px-4 py-2">Nama</th>
+						<th class="px-4 py-2">Email</th>
+						<th class="px-4 py-2">Status</th>
+						<th class="px-4 py-2">Aksi</th>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
+
+		<div class="overflow-x-auto">
+			<table id="" class="min-w-full table-auto table-info">
 				<thead class="bg-gray-100">
 					<tr>
 						<th class="px-4 py-2">No</th>
--- assets/vendor/js/datatable/datatable.js
+++ assets/vendor/js/datatable/datatable.js
@@ -1,3 +1,4 @@
+// with export button
 $(document).ready(function () {
 	$("table.table-data").DataTable({
 		responsive: true,
@@ -49,5 +50,31 @@ $(document).ready(function () {
 				],
 			},
 		],
+	});
+});
+
+// without export button
+$(document).ready(function () {
+	 $("table.table-info").DataTable({
+		responsive: true,
+		select: true,
+		searching: true,
+		paging: true,
+		pagingType: "full",
+		language: {
+			searchPlaceholder: "Cari...",
+			lengthMenu: "_MENU_ Data per halaman",
+			info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
+			infoEmpty: "Tidak ada data tersedia",
+			infoFiltered: "(filtered from _MAX_ Total data)",
+		},
+		lengthMenu: [
+			[5, 10, 25, 50, 100, 200, -1],
+			[5, 10, 25, 50, 100, 200, "All"],
+		],
+		dom:
+			"<'flex justify-between items-center mb-4 w-full text-gray-700'<'flex items-center w-2/3 sm:w-3/4 text-xs sm:text-sm'l><'flex items-center space-x-2'f>>" +
+			"<'w-full text-gray-700'tr>" +
+			"<'flex justify-between items-center my-4 w-full text-gray-700'<'flex items-center space-x-2 text-xs sm:text-sm'l><'flex items-center text-xs sm:text-sm'i><'flex items-center'p>>",
 	});
 });
